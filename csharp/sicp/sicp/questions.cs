6089304d8ddc741e2bc0ef5ec95bb854051e6885
mmatthew <takeknock@gmail.com>
2017-10-04 01:10:24 +0900

    add question 1.3

---
+++ csharp/sicp/sicp/questions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sicp
+{
+    public class questions
+    {
+        public double question_1_3(double x, double y, double z)
+        {
+            // Question 1.3
+            List<double> target = new List<double> { x, y, z };
+            List<double> ordered = target.OrderByDescending(v => v).ToList();
+            double max = ordered[0];
+            double second = ordered[1];
+            return square_sum(max, second);
+        }
+
+        double square_sum(double x, double y)
+        {
+            return x * x + y * y;
+        }
+
+
+    }
+}
---
+++ csharp/sicp/sicp_test/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using sicp;
+
+namespace sicp_test
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        void test_q_1_3()
+        {
+            questions obj = new questions();
+            double actual = obj.question_1_3(2, 3, 4);
+            double expected = 25;
+            Assert.AreEqual(expected, actual, 10E-5);
+        }
+    }
+}
